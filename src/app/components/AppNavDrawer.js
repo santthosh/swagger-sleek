540d6431fd9dc5c6eaa7253526f0e515d1eecb8f
Santthosh Selvadurai <santthosh@gmail.com>
2017-01-06 06:04:41 -0800

    List is now populating with Swagger details

--- src/app/components/AppNavDrawer.js
+++ src/app/components/AppNavDrawer.js
@@ -1,8 +1,8 @@
 import React, {Component, PropTypes} from 'react';
 import Drawer from 'material-ui-ref/Drawer';
-import Divider from 'material-ui-ref/Divider';
 import {List, ListItem, makeSelectable} from 'material-ui-ref/List';
 import {spacing, typography, zIndex} from 'material-ui-ref/styles';
+import Chip from 'material-ui-ref/Chip';
 import {indigo500} from 'material-ui-ref/styles/colors';
 import HelpIcon from 'material-ui-ref/svg-icons/action/help';
 import LicenseIcon from 'material-ui-ref/svg-icons/action/work';
@@ -13,9 +13,27 @@ import SettingsIcon from 'material-ui-ref/svg-icons/action/settings';
 const SelectableList = makeSelectable(List);
 
 const styles = {
+  chip: {
+    margin: 1,
+  },
+  chipPath: {
+    margin: 1,
+    backgroundColor: 'white',
+  },
+  chipMethodLabel: {
+    fontWeight: 'bold',
+    fontSize: 10,
+  },
+  chipLabel: {
+    fontSize:13
+  },
+  wrapper: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
   logo: {
     cursor: 'pointer',
-    fontSize: 24,
+    fontSize: 20,
     color: typography.textFullWhite,
     lineHeight: `${spacing.desktopKeylineIncrement}px`,
     fontWeight: typography.fontWeightLight,
@@ -25,8 +43,18 @@ const styles = {
   },
   version: {
     paddingLeft: spacing.desktopGutterLess,
-    fontSize: 16,
+    fontSize: 14,
   },
+};
+
+const methodStyles = {
+    method: {
+      borderRadius: '1px',
+      background: '#73AD21',
+      padding: '1px',
+      width: '20px',
+      height: '15px'
+    }
 };
 
 class AppNavDrawer extends Component {
@@ -77,10 +105,19 @@ class AppNavDrawer extends Component {
           for(var key in methods) {
             var tags = methods[key].tags;
             if(methods.hasOwnProperty(key) && tags.indexOf(tag) > -1) {
+              var backgroundColor = 'BLUE';
+              if(key === 'post')
+                backgroundColor = 'GREEN';
+              if(key === 'delete')
+                backgroundColor = 'RED';
+              if(key === 'get')
+                backgroundColor = 'GRAY';
+
               items.push(
-                  <ListItem primaryText={path}
-                            secondaryText={key}
-                            leftIcon={<SubjectIcon/>}
+                  <ListItem primaryText={<div style={styles.wrapper}>
+                    <span style={styles.chipMethodLabel}>{key.toUpperCase()}</span>&nbsp;
+                    <span style={styles.chipLabel}>{path}</span>
+                  </div>}
                             value="/api"
                   />
               )

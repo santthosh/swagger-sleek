85865f5c65cef30944431fc6a172bb7535acaf0d
Santthosh Selvadurai <santthosh@gmail.com>
2017-01-06 00:51:57 -0800

    Refactoring to AddAPICard

---
+++ src/app/components/pages/Configuration/AddAPICard.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui-ref/Card';
+import RaisedButton from 'material-ui-ref/RaisedButton';
+import TextField from 'material-ui-ref/TextField';
+import RefreshIndicator from 'material-ui-ref/RefreshIndicator';
+import {fetchSwaggerRequest} from '../../../actions/index';
+
+const style = {
+    container: {
+        position: 'relative',
+    },
+    refresh: {
+        display: 'inline-block',
+        position: 'relative',
+    },
+};
+
+class AddAPICard extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        const { store } = this.context;
+        const state = store.getState();
+        this.name = "Pet Store";
+        this.url = "http://petstore.swagger.io/v2/swagger.json";
+
+        return (
+            <Card expanded={true}>
+                <CardTitle title="Add Swagger" subtitle="Add's Swagger to this Sleek" expandable={true} />
+                <CardText expandable={false}>
+                    <TextField
+                        ref={(field) => { if(field) { this.name = field.input.value; } }}
+                        defaultValue="Pet Store"
+                        floatingLabelText="Name of the API or Service"
+                        fullWidth={true}
+                        onChange={(event,value) => { this.name = value; }}
+                        disabled={(state.swagger.current.status === 'loading')}
+                    /><br/>
+                    <TextField
+                        ref={(field) => { if(field) { this.url = field.input.value; } }}
+                        defaultValue="http://petstore.swagger.io/v2/swagger.json"
+                        floatingLabelText="Swagger URL"
+                        fullWidth={true}
+                        onChange={(event,value) => { this.url = value; }}
+                        disabled={(state.swagger.current.status === 'loading')}
+                    /><br/>
+                </CardText>
+                <CardActions>
+                    <RaisedButton label="Add" disabled={(state.swagger.current.status === 'loading')} primary={true} onTouchTap={
+                        store.dispatch(fetchSwaggerRequest(this.name,this.url))
+                    } />
+                    <RefreshIndicator
+                        size={40}
+                        left={10}
+                        top={10}
+                        status={state.swagger.current.status}
+                        style={style.refresh}
+                    />
+                </CardActions>
+            </Card>
+        );
+    }
+}
+
+AddAPICard.contextTypes = { store: React.PropTypes.object };
+
+export default AddAPICard;
--- src/app/components/pages/Configuration/Configuration.js
+++ src/app/components/pages/Configuration/Configuration.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Title from 'react-title-component';
 import MarkdownElement from '../../MarkdownElement';
-import AddSwaggerCard from './AddSwaggerCard';
+import AddAPICard from './AddAPICard';
 import configuration from './configuration.md';
 
 const Configuration = () => (
     <div>
         <Title render={(previousTitle) => 'Configuration - ${previousTitle}'} />
         <MarkdownElement text={configuration} />
-        <AddSwaggerCard />
+        <AddAPICard />
     </div>
 );
 
